42581a1a561df9cf8e896d44d1770b7c88f891a5
Julius <julius.kreutz@dexperio.net>
2020-09-09 22:56:21 +0200

    animation counter

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimationController : MonoBehaviour
 {
+    private int counter;
+
+    [SerializeField] private Text text;
+
+    private int max = 11;
 
     public void next()
     {
         Animator animator = FindObjectOfType<Animator>();
 
         if (animator == null) return;
+
+        if (animator.GetBool("next")) return;
         
+        counter = counter == max ? 0 : counter + 1;
+
         animator.SetTrigger("next");
+
+        text.text = "" + counter;
     }
 
     public void prev()
@@ -18,7 +30,12 @@ public class AnimationController : MonoBehaviour
 
         if (animator == null) return;
         
-        animator.SetTrigger("prev");
-    }
+        if (animator.GetBool("prev")) return;
 
+        counter = counter == 0 ? max : counter - 1;
+
+        animator.SetTrigger("prev");
+
+        text.text = "" + counter;
+    }
 }
