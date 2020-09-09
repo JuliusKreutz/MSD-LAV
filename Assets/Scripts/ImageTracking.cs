95f20664b0b40c9dbf8249abc7efc07324bdd3fd
Julius <julius.kreutz@dexperio.net>
2020-09-09 16:07:15 +0200

    Initial commit

---
+++ Assets/Scripts/ImageTracking.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+public class ImageTracking : MonoBehaviour
+{
+
+    private ARTrackedImageManager imageManager;
+
+    private void Awake()
+    {
+        imageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        imageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable()
+    {
+        imageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        
+    }
+}
